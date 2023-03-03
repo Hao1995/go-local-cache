59330b54cbaeade1b2315be38174c8b48b7d042a
Harry Lu <harry.nan.825@gmail.com>
2023-03-03 12:01:17 +0800

    test: check set data successfully and data could be expired

--- localcache/cache_test.go
+++ localcache/cache_test.go
@@ -75,8 +75,11 @@ func TestCacheExpiration(t *testing.T) {
 	cache := New(1)
 
 	cache.Set("key", "value")
+	value, ok := cache.Get("key")
+	assert.True(t, ok)
+	assert.Equal(t, "value", value)
 	time.Sleep(2 * time.Second)
 
-	_, ok := cache.Get("key")
+	_, ok = cache.Get("key")
 	assert.False(t, ok)
 }
